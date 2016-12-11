3439cbeac2f4ec4731a3ae250f4f19c64239c6f9
Li Wang <wangli1426@gmail.com>
2016-12-11 18:21:57 +0800

    Cite is implemented

---
+++ src/main/java/edu/illinois/adsc/Car.java
@@ -0,0 +1,15 @@
+package edu.illinois.adsc;
+
+/**
+ * Created by Robert on 12/11/16.
+ */
+public class Car {
+    long id;
+    double x;
+    double y;
+    public Car(int id, double x, double y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/main/java/edu/illinois/adsc/City.java
+++ src/main/java/edu/illinois/adsc/City.java
@@ -1,5 +1,9 @@
 package edu.illinois.adsc;
 
+import java.util.List;
+
+import java.util.*;
+
 /**
  * Created by Robert on 12/11/16.
  */
@@ -9,6 +13,7 @@ public class City {
     private RangePartition xRangePartition;
     private RangePartition yRangePartition;
     ZOrderCoding zOrderCoding;
+    Map<Integer, Set<Car>> zcode2Cars = new HashMap<>();
     public City(double x1, double x2, double y1, double y2, int partitions) {
         if(x1 >= x2 || y1 > y2 || partitions < 0)
             throw new IllegalArgumentException("Illegal arguments of City.");
@@ -26,7 +31,7 @@ public class City {
         return zOrderCoding.getZCode(xRangePartition.getPartition(x), yRangePartition.getPartition(y));
     }
 
-    public Intervals getZCodeIntervalsUnderARectagle(double x1, double x2, double y1, double y2) {
+    public Intervals getZCodeIntervalsInARectagle(double x1, double x2, double y1, double y2) {
         if(x1 > x2 || y1 > y2)
             throw new IllegalArgumentException("Illegal argument for a rectangle.");
         final int xPartition1 = xRangePartition.getPartition(x1);
@@ -36,5 +41,23 @@ public class City {
         return zOrderCoding.getIntervalsOfCodesInRectangle(xPartition1, xPartition2, yPartition1, yPartition2);
     }
 
+    public void store(Car car) {
+        int code = getZCodeForALocation(car.x, car.y);
+        if (!zcode2Cars.containsKey(code)) {
+            zcode2Cars.put(code, new HashSet<>());
+        }
+        zcode2Cars.get(code).add(car);
+    }
 
+    List<Car> getCarsInARectangle(double x1, double x2, double y1, double y2) {
+        Intervals intervals = getZCodeIntervalsInARectagle(x1, x2, y1, y2);
+        List<Car> cars = new ArrayList<>();
+        for(Interval interval: intervals.intervals) {
+            for (int code = interval.low; code <= interval.high; code++) {
+                if (zcode2Cars.containsKey(code))
+                    cars.addAll(zcode2Cars.get(code));
+            }
+        }
+        return cars;
+    }
 }
